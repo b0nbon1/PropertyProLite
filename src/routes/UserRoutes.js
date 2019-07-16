3efdf420997cb14ce270a85377d637deb85aa982
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-16 19:23:41 +0300

    feature(routes):add route login[finishes #167278956]

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -6,6 +6,7 @@ import Res from '../utils/helpers/responses';
 const route = express.Router();
 
 route.post('/auth/signup', Validation.user, Authentication.register);
+route.post('/auth/login', Validation.login, Authentication.loginUser);
 route.all('/auth/:i(signup|login)', (req, res) => Res.handleError(405, 'Invalid Method', res));
 
 export default route;
