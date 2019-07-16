71c32b4f580af579ed53f439d697ff13711e4300
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-07-16 08:46:26 +0300

    feature(helper):validate user details[delivers #167278986]

---
+++ src/utils/middleware/validators.js
@@ -0,0 +1,32 @@
+import Res from '../helpers/responses';
+import Regex from '../helpers/Regexes';
+import User from '../../Models/UsersModel';
+
+export default class Validations {
+    static async user(req, res, next) {
+        try {
+            const {
+                firstname,
+                lastname,
+                address,
+                email,
+                phoneNumber,
+                password,
+            } = req.body;
+            if (!firstname || !lastname || !address || !email || !phoneNumber || !password) {
+                return Res.handleError(400, 'Please fill all the fields', res);
+            }
+            if (await Regex.nameCheck(firstname)) return Res.handleError(400, 'enter valid firstname', res);
+            if (await Regex.nameCheck(lastname)) return Res.handleError(400, 'enter valid lastname', res);
+            if (await Regex.addressCheck(address)) return Res.handleError(400, 'address validation failed', res);
+            if (await Regex.phoneCheck(phoneNumber)) return Res.handleError(400, 'Enter valid phone Number', res);
+            if (await Regex.passCheck(password)) return Res.handleError(400, 'enter valid password. should be 6 character and more and contain letters and numbers', res);
+            if (await Regex.emailCheck(email)) return Res.handleError(400, 'enter valid email e.g user@gmail.com', res);
+            const user = await User.getUser(email);
+            if (await user.length !== 0) return Res.handleError(409, 'email account exists', res);
+            next();
+        } catch (error) {
+            return Res.handleError(500, error.toString(), res);
+        }
+    }
+}
